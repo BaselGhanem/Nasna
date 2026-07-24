01ecd66bdf32645423cefd91b6c78fb85d72ad22
Basel Ghanem <baselghanem21@gmail.com>
2026-07-25 01:28:31 +0300

    Add isolated Firestore configuration for NASNA modules

---
+++ firestore-config.js
@@ -0,0 +1,13 @@
+import { initializeFirestore } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+import { firebaseApp } from "./firebase-config.js?v=20260725.3";
+
+const firestoreDatabaseId = `ai-studio-2f881b3f-5867-4dfd-b360-c85f26c6ded4`;
+const db = initializeFirestore(
+  firebaseApp,
+  {
+    experimentalForceLongPolling: true
+  },
+  firestoreDatabaseId
+);
+
+export { db, firestoreDatabaseId };
