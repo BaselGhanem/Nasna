939546ed342230e892995c71e987d989d8d02dfc
Basel Ghanem <baselghanem21@gmail.com>
2026-07-24 16:59:50 +0300

    Build Stage 04 company and access foundation

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,18 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
 
-const firebaseConfig = {
+const firebaseConfig = Object.freeze({
   apiKey: `AIzaSyDP6FlbjAeUqut_79qW0T3rPSvfsjPdHfA`,
   authDomain: `gen-lang-client-0023266031.firebaseapp.com`,
   projectId: `gen-lang-client-0023266031`,
   storageBucket: `gen-lang-client-0023266031.firebasestorage.app`,
   messagingSenderId: `1052398695491`,
   appId: `1:1052398695491:web:77fe50eb0d8e3d1ec510f6`
-};
+});
 
 const firebaseApp = initializeApp(firebaseConfig);
 const auth = getAuth(firebaseApp);
+const db = getFirestore(firebaseApp);
 
-export { auth, firebaseApp };
+export { auth, db, firebaseApp, firebaseConfig };
