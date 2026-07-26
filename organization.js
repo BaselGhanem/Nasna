eb5435dbc6b89ec4d4cb8701f383c69ffc77b945
Basel Ghanem <baselghanem21@gmail.com>
2026-07-26 18:31:01 +0300

    Stage 10: update organization.js

--- organization.js
+++ organization.js
@@ -23,8 +23,8 @@ import {
 import {
   auth,
   firebaseConfig
-} from "./firebase-config.js?v=20260726.1";
-import { db } from "./firestore-config.js?v=20260726.1";
+} from "./firebase-config.js?v=20260726.3";
+import { db } from "./firestore-config.js?v=20260726.3";
 
 const translations = {
   en: {
@@ -209,7 +209,7 @@ const translations = {
   }
 };
 
-const version = `20260726.1`;
+const version = `20260726.3`;
 const languageKey = `nasna-language`;
 const roleValues = [`super_admin`, `hr_admin`, `employee`];
 const provisionableRoles = new Set([`super_admin`, `hr_admin`]);
