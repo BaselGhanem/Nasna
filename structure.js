a19826a123e5d387581b10358ecdfb944b0763be
Basel Ghanem <baselghanem21@gmail.com>
2026-07-26 16:31:43 +0300

    Stages 08-09: update structure.js

--- structure.js
+++ structure.js
@@ -7,10 +7,10 @@ import {
   serverTimestamp,
   writeBatch
 } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
-import { auth } from "./firebase-config.js?v=20260725.9";
-import { db } from "./firestore-config.js?v=20260725.9";
+import { auth } from "./firebase-config.js?v=20260726.1";
+import { db } from "./firestore-config.js?v=20260726.1";
 
-const version = `20260725.9`;
+const version = `20260726.1`;
 const languageKey = `nasna-language`;
 const adminRoles = new Set([`super_admin`, `hr_admin`]);
 const validStatuses = new Set([`active`, `inactive`]);
